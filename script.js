4a9a55005a79c963cae2d504ec6ea81d92354eff
Luis Felipe Francisco <110519969+LuisFelipeFrancisco@users.noreply.github.com>
2022-11-17 20:34:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+document.body.addEventListener('keyup', (event)=>{
+    playSound(event.code.toLowerCase());
+    console.log(event.code);
+});
+
+document.querySelector('.composer button').addEventListener('click', ()=>{
+    let song = document.querySelector('#input').value;
+
+    if(song !== ''){
+        let songArray = song.split('');
+        playComposition(songArray);
+    }
+});
+
+function playSound(sound){
+    let audioElement = document.querySelector(`#s_${sound}`);
+    let keyElement = document.querySelector(`div[data-key="${sound}"]`);
+    if(audioElement){
+        audioElement.currentTime = 0;
+        audioElement.play();
+    }
+    if(keyElement){
+        keyElement.classList.add('active');
+        setTimeout(()=>{
+            keyElement.classList.remove('active');
+        }, 300);
+    }
+}
+
+function playComposition(songArray){
+    let wait = 0;
+    for(let songItem of songArray){
+        setTimeout(()=>{
+            playSound(`key${songItem}`);
+        }, wait);
+        wait += 250;
+    }
+}
